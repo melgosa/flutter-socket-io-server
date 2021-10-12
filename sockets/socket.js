e3c1fd7ae1f616cc21de73f4ee2de83f23ead562
MarcosMelgosa <melgosa.kent@gmail.com>
2021-10-12 18:33:27 -0500

    Lógica de bandas agregada

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,20 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Queen'));
+bands.addBand( new Band('Bon Jovi'));
+bands.addBand( new Band('Héroes del Silencio'));
+bands.addBand( new Band('Metallica'));
+
 
 //Mensajes de sockets
 io.on('connection', client => {
     console.log('Cliente Conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => { 
         console.log('Cliente Desconectado');
@@ -13,4 +25,28 @@ io.on('connection', client => {
 
         io.emit('mensaje', {admin: 'Nuevo mensaje from server'});
      });
+
+     //Escuchar cuando el cliente (tablet, navegador, celular, etc.) emita el mensaje vote-band
+     client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        //Se le envia a todos los clientrs conectados para que tengan los cambios
+        //mas recientes, incluido el mismo cliente que votó
+        io.emit('active-bands', bands.getBands());
+     });
+
+     client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name));
+      //bands.voteBand(payload.id);
+      //Se le envia a todos los clientrs conectados para que tengan los cambios
+      //mas recientes, incluido el mismo cliente que votó
+      io.emit('active-bands', bands.getBands());
+      });
+
+      client.on('delete-band', (payload) => {
+         bands.deleteBand(payload.id);
+       //bands.voteBand(payload.id);
+       //Se le envia a todos los clientrs conectados para que tengan los cambios
+       //mas recientes, incluido el mismo cliente que votó
+       io.emit('active-bands', bands.getBands());
+       });
   });
